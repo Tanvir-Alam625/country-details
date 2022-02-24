7216b6202460ad0853724ac6247465a313256edc
tanveer <mdtanviralamht731@gmail.com>
2022-02-24 23:37:32 +0600

    details added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const main = document.getElementById('main');
-// console.log(main);
+const mainDetail = document.getElementById('detail')
+// api data load 
 const apiDataLoad = ()=>{
     const dataUrl = `https://restcountries.com/v3.1/all`;
     fetch(dataUrl)
     .then(res => res.json())
     .then(data => getData(data));
 }
+// get value 
 const getData = (countries) => {
     countries.forEach(country => {
         const div = document.createElement('div');
@@ -13,14 +15,26 @@ const getData = (countries) => {
         div.innerHTML = `
         <h3 class="country-name text-4xl font-bold mb-2">${country.name.common}</h3>
         <p class="mb-2 text-2xl">${country.capital}</p>
-        <a href="./details.html">
-        <button onclick="details('${country.name.common}')" class= "bg-white rounded-lg text-2xl py-1 px-2 border-2">See Details</button></a>
-        `
-        main.appendChild(div);
+        <button onclick="details('${country.name.common}')" class= "bg-white rounded-lg text-2xl py-1 px-2 border-2">See Details</button>
         
+        `;
+        main.appendChild(div);
     });
 }
+// show the details 
 const details =(name) =>{
-    console.log(name);
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getDetails(data[0]))
 } 
+const getDetails = (name)=>{
+    
+    const detail = document.createElement('div');
+    detail.innerHTML = `
+    <h2 class="country-name text-4xl font-bold mb-2">${name.capital}</h2>
+    `;
+    console.log(name.capital);
+    // mainDetail.appendChild(detail);
+}
 apiDataLoad();
